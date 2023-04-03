5bedcb147646b3e40c6428b7cace24b4ec65c21f
JozywanRamos22 <suporte@jozywan1995@gmail.com>
2023-04-03 20:14:19 -0300

    Projeto-node

---
+++ index.js
@@ -0,0 +1,95 @@
+
+const { request, response } = require('express')
+const express = require('express')
+const uuid = require('uuid')
+
+
+const port = 3001
+const app = express()
+app.use(express.json())
+
+
+
+const users = []
+
+const checkUserId = (request, response, next) => {
+    const { id } = request.params
+
+    const index = users.findIndex(user => user.id === id)   // procurando o usuario dentro da mihha array, me retornando a posição do meu usuario
+
+    if (index < 0) {
+        return response.status(404).json({ error: "User not found" })
+
+    }
+    request.userIndex = index
+    request.userId = id 
+
+    next() // essa é uma estrutura "Middleware"
+}
+
+
+
+
+
+
+
+
+app.get('/users', (request, response) => {
+
+
+    return response.json(users)  
+})
+
+app.post('/users', (request, response) => {
+    const { name, age } = request.body
+
+    const user = { id: uuid.v4(), name, age }
+
+    users.push(user)
+
+    return response.status(201).json(user)
+
+})
+
+app.put('/users/:id', checkUserId, (request, response) => {
+
+    const { name, age } = request.body    // trazendo as inforçoes atualziadas
+
+    const index = request.userIndex 
+    const id = request.userId
+
+    const updateUser = { id, name, age }  // usuario sendo atualizado
+
+
+    users[index] = updateUser  // atualizando, e mostrando na tela o meu úsuario sendo atualizado
+
+
+
+
+    return response.json(updateUser)
+})
+
+
+
+app.delete('/users/:id', checkUserId, (request, response) => {
+    const index = request.userIndex 
+
+
+
+    users.splice(index, 1)
+
+    return response.status(204).json()
+
+})
+
+
+
+
+
+
+
+
+app.listen(3001, () => {
+    console.log('🚀Serven started on port 3001🚀')
+})  // porta de rota no meu caso eu to ultizando a porta de rota 3001 
+//por que a porta 3000 ta sendo ultilizada
